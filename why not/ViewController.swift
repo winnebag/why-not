83bd5faee305f81b0c66638af507f9afcd76faad
Leeroy DSouza <leeroydsouza@Leeroys-MacBook-Pro.local>
2016-10-11 05:36:42 -0500

    Initial Commit

---
+++ why not/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  why not
+//
+//  Created by Leeroy DSouza on 10/11/16.
+//  Copyright © 2016 Leeroy DSouza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
